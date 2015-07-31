0dbfdea51dc41e92c408c3deda00990e3efba298
Sarah Foregger <sarahfo@Sarahs-MacBook-Pro-2.local>
2015-07-30 18:12:39 -0700

    created GoodReads searchrepo

---
+++ bookgetter.js
@@ -0,0 +1,21 @@
+URL: https://www.goodreads.com/review/list.xml?v=2    (sample url) 
+HTTP method: GET 
+Parameters: 
+v: 2
+id: Goodreads id of the user
+shelf: read, currently-reading, to-read, etc. (optional)
+sort: title, author, cover, rating, year_pub, date_pub, date_pub_edition, date_started, date_read, date_updated, date_added, recommender, avg_rating, num_ratings, review, read_count, votes, random, comments, notes, isbn, isbn13, asin, num_pages, format, position, shelves, owned, date_purchased, purchase_location, condition (optional)
+search[query]: query text to match against member's books (optional)
+order: a, d (optional)
+page: 1-N (optional)
+per_page: 1-200 (optional)
+key: Developer key (required).;
+'
+
+var bookList =  {
+	var url = URL
+	id: userID
+	shelf: to-read
+
+	}
+}
